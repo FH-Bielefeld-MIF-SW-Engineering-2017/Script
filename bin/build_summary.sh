a99c2b1add9cc476e2ce3c5a434c95d2b74bb98f
Christian Holzberger <ch@mosaiksoftware.de>
2017-05-07 21:31:00 +0200

    script zum automatischen generieren des summarys

--- bin/build_summary.sh
+++ bin/build_summary.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ls ../src/*.md | while read i; do
+cd ..
+echo "# Summary" > SUMMARY_AUTO.md
+ls src/*.md | while read i; do
 
-echo "Headlines in: $i"
-grep  "^\#" "../src/$i"
+	grep  "^\#" "$i"
+	grep  "^\#" "$i" | while read j; do
+		TITLE=$(echo $j | sed -e "s/#### /    * \[/g" | sed -e "s/### /  * \[/g" | sed -e "s/## / * \[/g" | sed -e "s/# /* \[/" )
+		echo "$TITLE]($i)" >> SUMMARY_AUTO.md
+	done 
 done
